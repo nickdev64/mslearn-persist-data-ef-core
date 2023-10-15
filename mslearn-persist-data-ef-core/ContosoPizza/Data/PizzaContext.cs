4a3b17da35208e1c424a6bd5a773e0218635a11b
Nicola Martella <nicola.martella@nick4name.eu>
2023-10-15 11:20:05 +0200

    15/10/2023

---
+++ mslearn-persist-data-ef-core/ContosoPizza/Data/PizzaContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data;
+
+public class PizzaContext : DbContext
+{
+    public PizzaContext (DbContextOptions<PizzaContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSet<T> corrispondono alle tabelle da creare nel database
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+    public DbSet<Topping> Toppings => Set<Topping>();
+    public DbSet<Sauce> Sauces => Set<Sauce>();
+}
